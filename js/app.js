9e0577fffc99ebbf2cdbd5261b24e2b87d7a278d
Jette <jette@deskrock.ee>
2013-08-02 17:31:22 +0300

    currently not using my own javascript

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ window.onload = function(){
 
 var gallery = ['gallery-1.jpg','gallery-2.jpg', 'gallery-3.jpg']
 
- $.each(gallery,function(index, value){
+ // $.each(gallery, function(index, value){
 
-
- });
+ // });
 
 }
