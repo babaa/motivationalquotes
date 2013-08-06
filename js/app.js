83201e07c0ecaa1a7177def5386983ca5c5311c0
Jette <jette@deskrock.ee>
2013-08-06 12:59:16 +0300

    host images locally

--- js/app.js
+++ js/app.js
@@ -7,15 +7,15 @@ window.onload = function() {
   });
 
 var gallery = {
-'http://i.imgur.com/rNZ6Ios.jpg':'see the world',
-'http://i.imgur.com/sw7paXH.jpg' :'enjoy the little things in life',
-'http://i.imgur.com/L90qkcS.png':'always do what you love, never settle',
-'http://i.imgur.com/MpCzc.jpg': 'do more of what makes you happy',
-'http://i.imgur.com/y37o4.jpg' : 'good things will come my way',
-'http://i.imgur.com/SnoVEHd.jpg' : 'being happy is productive',
-'http://i.imgur.com/M8tLJT5.jpg' : 'just start',
-'http://i.imgur.com/r2kb023.jpg' : 'nothing is impossible for you, my dear',
-'http://i.imgur.com/Cu4BqeL.jpg' : 'make today ridiculously amazing'
+'img/rNZ6Ios.jpg':'see the world',
+'img/sw7paXH.jpg' :'enjoy the little things in life',
+'img/L90qkcS.png':'always do what you love, never settle',
+'img/MpCzc.jpg': 'do more of what makes you happy',
+'img/y37o4.jpg' : 'good things will come my way',
+'img/SnoVEHd.jpg' : 'being happy is productive',
+'img/M8tLJT5.jpg' : 'just start',
+'img/r2kb023.jpg' : 'nothing is impossible for you, my dear',
+'img/Cu4BqeL.jpg' : 'make today ridiculously amazing'
 };
 
 var shuffled_gallery = shuffleProperties(gallery);
